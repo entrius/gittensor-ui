6b39cd0470ed75a8b6c6a970d1bc056b5ae373db
grant <granteldeir@gmail.com>
2025-12-03 18:34:26 -0600

    Fix: Correct JSX syntax errors in OG image generator

--- netlify/edge-functions/og-image-miner.ts
+++ netlify/edge-functions/og-image-miner.ts
@@ -45,9 +45,8 @@ export default async (req: Request) => {
     }
 
     return new ImageResponse(
-        (
-            <div
-        style= {{
+        <div
+      style={{
         height: "100%",
         width: "100%",
         display: "flex",
@@ -57,112 +56,104 @@ export default async (req: Request) => {
         backgroundColor: "#000",
         backgroundImage: "linear-gradient(to bottom right, #000 0%, #1a1a1a 100%)",
     }}
+    >
+    <div
+        style={
+    {
+        display: "flex",
+            position: "absolute",
+                top: 40,
+                    left: 60,
+                        fontSize: 32,
+                            fontWeight: 700,
+                                color: "#fff",
+                                    fontFamily: "JetBrains Mono, monospace",
+        }
+}
       >
-    {/* Gittensor Logo/Title */ }
-    < div
-style = {{
-    display: "flex",
-        position: "absolute",
-            top: 40,
-                left: 60,
-                    fontSize: 32,
-                        fontWeight: 700,
-                            color: "#fff",
-                                fontFamily: "JetBrains Mono, monospace",
-          }}
-        >
     GITTENSOR
     </div>
 
-{/* Main Content */ }
-<div
-          style={
-    {
-        display: "flex",
-            flexDirection: "row",
-                alignItems: "center",
-                    gap: 60,
-          }
-}
-        >
-    {/* GitHub Avatar */ }
-    < img
-src = { avatarUrl }
+    < div
+style = {{
+    display: "flex",
+        flexDirection: "row",
+            alignItems: "center",
+                gap: 60,
+        }}
+      >
+    <img
+          src={ avatarUrl }
 width = { 200}
 height = { 200}
 style = {{
     borderRadius: 100,
         border: "4px solid #00ffff",
-            }}
-          />
+          }}
+        />
 
-{/* Stats Section */ }
-<div
-            style={
-    {
-        display: "flex",
-            flexDirection: "column",
-                gap: 20,
-            }
-}
-          >
-    {/* Username and Rank */ }
     < div
 style = {{
     display: "flex",
-        fontSize: 48,
-            fontWeight: 700,
-                color: "#fff",
-                    fontFamily: "JetBrains Mono, monospace",
-              }}
-            >
+        flexDirection: "column",
+            gap: 20,
+          }}
+        >
+    <div
+            style={
+    {
+        display: "flex",
+            fontSize: 48,
+                fontWeight: 700,
+                    color: "#fff",
+                        fontFamily: "JetBrains Mono, monospace",
+            }
+}
+          >
     { username }
 {
     rank && (
         <span
-                  style={
+                style={
         {
             fontSize: 48,
                 fontWeight: 700,
                     color: "#00ffff",
                         marginLeft: 20,
-                  }
+                }
     }
-                >
+              >
         { rank }
         </span>
-              )
+            )
 }
 </div>
 
-{/* Stats Grid */ }
-<div
+    < div
+style = {{
+    display: "flex",
+        flexDirection: "row",
+            gap: 40,
+            }}
+          >
+    <div
               style={
     {
         display: "flex",
-            flexDirection: "row",
-                gap: 40,
+            flexDirection: "column",
               }
 }
             >
-    <div
-                style={
-    {
-        display: "flex",
-            flexDirection: "column",
-                }
-}
-              >
     <span
-                  style={
+                style={
     {
         fontSize: 14,
             color: "#888",
                 fontFamily: "JetBrains Mono, monospace",
                     textTransform: "uppercase",
-                  }
+                }
 }
-                >
+              >
     SCORE
     </span>
     < span
@@ -171,8 +162,8 @@ style = {{
         fontWeight: 700,
             color: "#fff",
                 fontFamily: "JetBrains Mono, monospace",
-                  }}
-                >
+                }}
+              >
     { score }
     </span>
     </div>
@@ -181,18 +172,18 @@ style = {{
 style = {{
     display: "flex",
         flexDirection: "column",
-                }}
-              >
+              }}
+            >
     <span
-                  style={
+                style={
     {
         fontSize: 14,
             color: "#888",
                 fontFamily: "JetBrains Mono, monospace",
                     textTransform: "uppercase",
-                  }
+                }
 }
-                >
+              >
     PULL REQUESTS
         </span>
         < span
@@ -201,8 +192,8 @@ style = {{
         fontWeight: 700,
             color: "#fff",
                 fontFamily: "JetBrains Mono, monospace",
-                  }}
-                >
+                }}
+              >
     { prs }
     </span>
     </div>
@@ -211,18 +202,18 @@ style = {{
 style = {{
     display: "flex",
         flexDirection: "column",
-                }}
-              >
+              }}
+            >
     <span
-                  style={
+                style={
     {
         fontSize: 14,
             color: "#888",
                 fontFamily: "JetBrains Mono, monospace",
                     textTransform: "uppercase",
-                  }
+                }
 }
-                >
+              >
     LINES
     </span>
     < span
@@ -231,8 +222,8 @@ style = {{
         fontWeight: 700,
             color: "#0f0",
                 fontFamily: "JetBrains Mono, monospace",
-                  }}
-                >
+                }}
+              >
     +{ additions }
     </span>
     < span
@@ -241,16 +232,15 @@ style = {{
         fontWeight: 700,
             color: "#f00",
                 fontFamily: "JetBrains Mono, monospace",
-                  }}
-                >
+                }}
+              >
     -{ deletions }
     </span>
     </div>
     </div>
     </div>
     </div>
-    </div>
-    ),
+    </div>,
 {
     width: 1200,
         height: 630,
