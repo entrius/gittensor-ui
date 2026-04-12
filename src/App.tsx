a954348c29640364b5ee8bed8ded6e7c256931a7
Stackwyre <stackwyre@gmail.com>
2026-04-11 21:00:28 -0500

    Fix #161: /bounties URL returns 404 even though the sidebar button is labeled "bounties"

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,25 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { Toaster } from 'sonner';
-
-import Sidebar from './components/Sidebar';
+import Layout from './components/Layout';
 import Dashboard from './pages/Dashboard';
-import Miners from './pages/Miners';
 import Issues from './pages/Issues';
-import Search from './pages/Search';
-import Settings from './pages/Settings';
+import IssueDetails from './pages/IssueDetails';
+import Miners from './pages/Miners';
 import MinerDetails from './pages/MinerDetails';
 import RepositoryDetails from './pages/RepositoryDetails';
 import PRDetails from './pages/PRDetails';
-import IssueDetails from './pages/IssueDetails';
+import Repositories from './pages/Repositories';
 import FAQ from './components/FAQ';
-import NotFound from './pages/NotFound';
-import { ThemeProvider } from './contexts/ThemeContext';
-import { SidebarProvider } from './contexts/SidebarContext';
+import { Toaster } from 'react-hot-toast';
+import './App.css';
 
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       staleTime: 1000 * 60 * 5, // 5 minutes
-      gcTime: 1000 * 60 * 30, // 30 minutes
+      gcTime: 1000 * 60 * 10, // 10 minutes
     },
   },
 });
@@ -31,32 +27,25 @@ const queryClient = new QueryClient({
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <ThemeProvider>
-        <SidebarProvider>
-          <Router>
-            <div className="flex h-screen bg-gray-50 dark:bg-gray-900">
-              <Sidebar />
-              <main className="flex-1 overflow-hidden">
-                <Routes>
-                  <Route path="/" element={<Dashboard />} />
-                  <Route path="/miners" element={<Miners />} />
-                  <Route path="/miners/details" element={<MinerDetails />} />
-                  <Route path="/miners/repository" element={<RepositoryDetails />} />
-                  <Route path="/miners/pr" element={<PRDetails />} />
-                  <Route path="/issues" element={<Issues />} />
-                  <Route path="/bounties" element={<Issues />} />
-                  <Route path="/issues/details" element={<IssueDetails />} />
-                  <Route path="/search" element={<Search />} />
-                  <Route path="/settings" element={<Settings />} />
-                  <Route path="/faq" element={<FAQ />} />
-                  <Route path="*" element={<NotFound />} />
-                </Routes>
-              </main>
-            </div>
-            <Toaster position="bottom-right" richColors />
-          </Router>
-        </SidebarProvider>
-      </ThemeProvider>
+      <Router>
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<Dashboard />} />
+              <Route path="issues" element={<Issues />} />
+              <Route path="bounties" element={<Navigate to="/issues" replace />} />
+              <Route path="issues/details" element={<IssueDetails />} />
+              <Route path="miners" element={<Miners />} />
+              <Route path="miners/details" element={<MinerDetails />} />
+              <Route path="miners/repository" element={<RepositoryDetails />} />
+              <Route path="miners/pr" element={<PRDetails />} />
+              <Route path="repositories" element={<Repositories />} />
+              <Route path="faq" element={<FAQ />} />
+            </Route>
+          </Routes>
+          <Toaster position="top-right" />
+        </div>
+      </Router>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
