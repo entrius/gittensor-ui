dda5ef1d8cc10db8c5c97c8c40f1b1ba0556d5e9
ms1112 <milosdelic.tech@gmail.com>
2026-04-23 17:37:42 -0700

    feat(dashboard): add Featured Work cards for top PRs and issues (#742)

--- src/pages/dashboard/DashboardPage.tsx
+++ src/pages/dashboard/DashboardPage.tsx
@@ -6,6 +6,7 @@ import theme, { scrollbarSx } from '../../theme';
 import { type TrendTimeRange } from './dashboardData';
 import useDashboardData from './useDashboardData';
 import ActiveNetwork from './views/ActiveNetwork';
+import DashboardFeaturedWorkSection from './views/DashboardFeaturedWork';
 import DashboardTopContributors from './views/DashboardTopContributors';
 import LiveSidebar from './views/LiveSidebar';
 
@@ -21,6 +22,7 @@ const DashboardFeaturePage: React.FC = () => {
     overview,
     trendLabels,
     trendSeries,
+    featuredWork,
     featuredContributors,
     featuredDiscoveryContributors,
     isLoading,
@@ -86,6 +88,11 @@ const DashboardFeaturePage: React.FC = () => {
             <DashboardTopContributors
               title="Featured Discoverers"
               contributors={featuredDiscoveryContributors}
+              isLoading={isLoading}
+            />
+
+            <DashboardFeaturedWorkSection
+              items={featuredWork}
               isLoading={isLoading}
             />
           </Box>
