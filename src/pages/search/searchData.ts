19a41546bf0626e25b5b750f820f48c1e238df4d
Ander <61125407+anderdc@users.noreply.github.com>
2026-05-08 11:37:11 -0500

    Read repository fields from nested config blob (#993)
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- src/pages/search/searchData.ts
+++ src/pages/search/searchData.ts
@@ -166,7 +166,7 @@ const buildRepoSearchData = (
       return {
         fullName: repo.fullName,
         owner: repo.owner,
-        weight: parseNumber(repo.weight),
+        weight: parseNumber(repo.config?.weight),
         totalScore: stats?.totalScore || 0,
         totalPRs: stats?.totalPRs || 0,
         contributors: stats?.uniqueAuthors.size || 0,
