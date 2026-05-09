cb1c968f33acc755e18549612edd62c3057db4a3
Watcharapon-T <watcharapon.thod@gmail.com>
2026-05-10 00:04:59 +0700

    fix: robust avatar resolution for leaderboard miners

--- src/components/leaderboard/LeaderboardSidebar.tsx
+++ src/components/leaderboard/LeaderboardSidebar.tsx
@@ -274,7 +274,7 @@ const LeaderboardRow: React.FC<LeaderboardRowProps> = ({
         }}
       >
         <Avatar
-          src={getGithubAvatarSrc(miner.author || miner.githubId)}
+          src={getGithubAvatarSrc(miner.author, miner.githubId)}
           alt={miner.author || miner.githubId}
           sx={{ width: 20, height: 20 }}
         />
